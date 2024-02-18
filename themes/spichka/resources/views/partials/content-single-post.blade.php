3adffac8622abc9d93766e6d004714831a9962f2
John Doe <john.doe@spichka.media>
2024-02-18 04:45:11 +0200

    Fix

---
+++ themes/spichka/resources/views/partials/content-single-post.blade.php
@@ -0,0 +1,42 @@
+<article>
+  <div class="container max-width-1140">
+    <div class="row">
+      <div class="col-sm-6">
+        @php(the_post_thumbnail('medium'))
+      </div>
+      <div class="col-sm-6">
+        <h1>
+          @php(the_title())
+        </h1>
+
+        <div>
+          {!! do_shortcode('[publishpress_authors_box layout="ppma_boxes_15606"]') !!}
+
+          <div id="post-categories">
+            {{ _e('Categories') }}:
+            @php(the_category(', '))
+          </div>
+
+          <div id="post-date">
+            <span>@svg('images.calendar-regular')</span>
+            @php(the_date('j F, Y'))
+          </div>
+        </div>
+
+        <div>
+          {!! do_shortcode('[ez-toc]') !!}
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container max-width-720 mt-5">
+    <div class="row">
+      <div class="col-xs-12">
+        @php(the_content())
+
+        @php(do_shortcode('[mistape format="text"]'))
+      </div>
+    </div>
+  </div>
+</article>
--- themes/spichka/resources/views/single.blade.php
+++ themes/spichka/resources/views/single.blade.php
@@ -3,47 +3,6 @@
 @section('content')
   @while (have_posts())
     @php(the_post())
-    <article>
-      <div class="container max-width-1140">
-        <div class="row">
-          <div class="col-sm-6">
-            @php(the_post_thumbnail('medium'))
-          </div>
-          <div class="col-sm-6">
-            <h1>
-              @php(the_title())
-            </h1>
-
-            <div>
-              {!! do_shortcode('[publishpress_authors_box layout="ppma_boxes_15606"]') !!}
-
-              <div id="post-categories">
-                {{ _e('Categories') }}:
-                @php(the_category(', '))
-              </div>
-
-              <div id="post-date">
-                <span>@svg('images.calendar-regular')</span>
-                @php(the_date('j F, Y'))
-              </div>
-            </div>
-
-            <div>
-              {!! do_shortcode('[ez-toc]') !!}
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="container max-width-720 mt-5">
-        <div class="row">
-          <div class="col-xs-12">
-            @php(the_content())
-
-            @php(do_shortcode('[mistape format="text"]'))
-          </div>
-        </div>
-      </div>
-    </article>
+    @includeFirst(['partials.content-single-' . get_post_type(), 'partials.content-single'])
   @endwhile
 @endsection
