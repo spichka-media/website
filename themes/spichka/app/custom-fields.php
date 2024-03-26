194013d8c2c4d6fabc3804221953f00f3d987432
John Doe <john.doe@spichka.media>
2024-03-26 09:00:49 +0200

    Добавил комментарии

--- themes/spichka/app/custom-fields.php
+++ themes/spichka/app/custom-fields.php
@@ -7,20 +7,28 @@ use Carbon_Fields\Carbon_Fields;
 
 add_action( 'carbon_fields_register_fields', function () {
     
-    Container::make( 'theme_options', __( 'Theme Options' ) )
+    Container::make( 'theme_options', __( 'Настройки темы' ) )
     ->add_fields( array(
         Field::make( 'text', 'theme_email', 'Email' )->set_default_value('info@spichka.media'),
-        Field::make( 'complex', 'theme_socials', 'Socials' )
+        Field::make( 'complex', 'theme_socials', 'Социальные сети' )
         ->set_layout( 'tabbed-horizontal' )
         ->add_fields( array(
-            Field::make( 'text', 'theme_social_link', 'Link' ),
-            Field::make( 'text', 'theme_social_icon', 'Icon' ),
+            Field::make( 'text', 'theme_social_link', 'Ссылка' ),
+            Field::make( 'text', 'theme_social_icon', 'Иконка' ),
         ) ),
+        Field::make( 'text', 'theme_telegram_channel', 'Телеграм канал' )->set_default_value( 'spichka_media' ),
         Field::make( 'image', 'theme_footer_image', 'Изображение' ),
-        Field::make( 'text', 'theme_footer_text', 'Footer text' )->set_default_value( 'Авторские права никак не защищены и принадлежат народу. Но всё равно: ссылайтесь на нас, когда копируете наши материалы.' ),
+        Field::make( 'text', 'theme_footer_text', 'Текст в футере' )->set_default_value( 'Авторские права никак не защищены и принадлежат народу. Но всё равно: ссылайтесь на нас, когда копируете наши материалы.' ),
     ) );
 
-    Container::make( 'post_meta', __( 'Front page options' ) )
+    Container::make( 'post_meta', __( 'Настройки поста' ) )
+    ->where( 'post_type', '=', 'post' )
+    ->add_tab( __( 'Комментарии' ), array(
+        Field::make( 'checkbox', 'post_show_comments', 'Отображать комментарии' )->set_option_value('yes'),
+        Field::make( 'text', 'post_telegram_post_id', __( 'Номер поста в Telegram' ) ),
+    ) );
+
+    Container::make( 'post_meta', __( 'Настройки главной страницы' ) )
     ->where( 'post_id', '=', get_option( 'page_on_front' ) )
     ->add_tab( __( 'Баннер' ), array(
         Field::make( 'text', 'front_banner_header', __( 'Заголовок' ) )->set_default_value( 'Марксистский журнал для умных, молодых и злых.'),
@@ -54,7 +62,7 @@ add_action( 'carbon_fields_register_fields', function () {
             Field::make( 'text', 'front_connect_blocks_block_form', 'Форма' ),
         ) ),
     ) )
-    ->add_tab( __( 'Donate' ), array(
+    ->add_tab( __( 'Донать' ), array(
         Field::make( 'text', 'front_donate_header', __( 'Заголовок' ) )->set_default_value( 'Помогай'),
         Field::make( 'text', 'front_donate_description', __( 'Описание' ) )->set_default_value( 'Мы работаем над проектом в свободное время и на энтузиазме. Чтобы творить, нам нужно тратить деньги на сайт, покупать оборудование для видео и подкастов, снимать студии. Спасибо, что присылаете нам деньги — так нам реже приходится тратить свои.'),
         Field::make( 'text', 'front_donate_button_text', __( 'Текст на кнопке' ) )->set_default_value( 'Задонатить'),
