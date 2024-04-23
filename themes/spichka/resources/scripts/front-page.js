092b84dca60a5322b43643e7dec691a378ec53d6
Ivan Kashtanov <ivan.kashtanov@spichka.media>
2024-04-23 08:08:47 +0300

    Revert "delete swiper library"
    This reverts commit bf4bfa2f142a6c38714efa50044a64ff1675e044.

--- themes/spichka/resources/scripts/front-page.js
+++ themes/spichka/resources/scripts/front-page.js
@@ -1,7 +1,30 @@
 import domReady from '@roots/sage/client/dom-ready';
 
+import Swiper from 'swiper';
+
 /**
  * Application entrypoint
  */
 domReady(async () => {
+  const sliders = document.querySelectorAll('.swiper');
+  const conf = {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    // Responsive breakpoints
+    breakpoints: {
+      // sm breakpoint
+      576: {
+        slidesPerView: 2,
+      },
+      // lg breakpoint
+      992: {
+        slidesPerView: 5,
+      },
+    },
+  };
+
+  sliders.forEach((item) => {
+    new Swiper(item, conf);
+  });
 });
