71147983f1608747e46ae9a50fc2040f7560fa19
Ivan Kashtanov <146159543+ivan-spichka@users.noreply.github.com>
2024-04-25 21:37:45 +0300

    Update blockquote, increase article's width (#11)

--- themes/spichka/resources/views/sections/header.blade.php
+++ themes/spichka/resources/views/sections/header.blade.php
@@ -2,7 +2,7 @@
 
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand fs-6" href="{{ home_url('/') }}">{!! $siteName !!}</a>
+      <a class="navbar-brand fw-bold" href="{{ home_url('/') }}">{!! $siteName !!}</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#headerMenu"
         aria-controls="headerMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
