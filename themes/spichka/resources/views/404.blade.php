5103a2657fe41b49e3dcc2f5d4f2684d62b28bfc
Ivan Kashtanov <146159543+ivan-spichka@users.noreply.github.com>
2024-05-05 21:15:37 +0300

    feat: fix layout, add commit hooks and prettier plugin (#19)

--- themes/spichka/resources/views/404.blade.php
+++ themes/spichka/resources/views/404.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="d-flex align-items-center justify-content-center bg-dark flex-grow-1">
+  <div
+    class="d-flex align-items-center justify-content-center bg-dark flex-grow-1">
     <h1 class="display-1 fw-bold text-white">{{ __('Page not found') }}</h1>
   </div>
 @endsection
--- themes/spichka/resources/views/components/post-card.blade.php
+++ themes/spichka/resources/views/components/post-card.blade.php
@@ -1,9 +1,7 @@
 <div class="post-card">
   <a href="{!! $url !!}">
     {!! $thumbnail !!}
-  </a>
 
-  <a href="{!! $url !!}">
-    <h3 class="mt-2">{!! $title !!}</h3>
+    <h6 class="mt-3 break-word">{!! $title !!}</h6>
   </a>
 </div>
