56c980a06fd4d3c669bd2964cdbab351a29c5fff
John Doe <john.doe@spichka.media>
2024-06-26 18:11:06 +0300

    fix: margins connect section

--- themes/spichka/resources/views/components/connect-card.blade.php
+++ themes/spichka/resources/views/components/connect-card.blade.php
@@ -3,20 +3,22 @@
   style="
   @if($color) color: {{ $color }}; @endif
   background-color: {!! $background_color !!}">
-  <div class="d-flex flex-column row-gap-5 card-body p-6">
-    <h3 class="card-title fw-bold">
+  <div class="d-flex flex-column card-body p-6">
+    <h3 class="card-title fw-bold mb-3">
       {!! $title !!}
     </h3>
-    <div class="fs-6">
+    <div class="description fs-6">
       {!! $description !!}
     </div>
 
     @if ($button_text)
-      <a
-        class="btn btn-outline-light fw-bold border-2 text-decoration-none mt-auto"
-        href="mailto:{{ carbon_get_theme_option('theme_email') }}">
-        {!! $button_text !!}
-      </a>
+      <div class="pt-3 pt-sm-10 mt-sm-auto">
+        <a
+          class="btn btn-outline-light fw-bold border-2 text-decoration-none w-100"
+          href="mailto:{{ carbon_get_theme_option('theme_email') }}">
+          {!! $button_text !!}
+        </a>
+      </div>
     @endif
   </div>
 </div>
