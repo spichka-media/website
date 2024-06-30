89672cfd8412683b10b27fc96eeb7e95e295d5ef
John Doe <john.doe@spichka.media>
2024-06-30 15:38:05 +0300

    feat: adjust wp_calculate_image_sizes

--- themes/spichka/app/filters.php
+++ themes/spichka/app/filters.php
@@ -108,7 +108,8 @@ add_filter('the_content', function ($content) {
 });
 
 add_filter('wp_calculate_image_sizes', function ($sizes) {
-  $sizes = '(max-width: 576px) 200px, (min-width: 577px) 500px';
+  $sizes =
+    '(max-width: 576px) 200px, (min-width: 577px) and (max-width: 767px) 500px, (min-width: 768px) 900px';
 
   return $sizes;
 });
