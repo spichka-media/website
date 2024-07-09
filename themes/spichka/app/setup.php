aa93d40dd275e4c7585f6123b79bc5fc3f2ff5cb
John Doe <john.doe@spichka.media>
2024-07-10 00:27:48 +0300

    feat: remove multiple authors css

--- themes/spichka/app/setup.php
+++ themes/spichka/app/setup.php
@@ -65,9 +65,6 @@ add_action(
     );
 
     wp_dequeue_style('wp-block-library'); // Remove WordPress core CSS
-
-    wp_dequeue_style('multiple-authors-widget-css');
-    wp_deregister_style('multiple-authors-widget-css');
   },
   100
 );
