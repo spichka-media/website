b7e54e67685c4e37e66ac0820ca2c43edf37b0b0
John Doe <john.doe@spichka.media>
2024-09-28 11:52:03 +0300

    feat: forbid to interact with wp rest api for non auth users

--- themes/spichka/app/filters.php
+++ themes/spichka/app/filters.php
@@ -185,3 +185,25 @@ add_filter(
   10,
   0
 );
+
+add_filter('rest_authentication_errors', function ($result) {
+  // If a previous authentication check was applied,
+  // pass that result along without modification.
+  if (true === $result || is_wp_error($result)) {
+    return $result;
+  }
+
+  // No authentication has been performed yet.
+  // Return an error if user is not logged in.
+  if (!is_user_logged_in()) {
+    return new WP_Error(
+      'rest_not_logged_in',
+      __('You are not currently logged in.'),
+      ['status' => 401]
+    );
+  }
+
+  // Our custom authentication check should have no effect
+  // on logged-in requests
+  return $result;
+});
