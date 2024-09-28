ce6fc944f17c30458707b7e54e8d65bc3b48a024
John Doe <john.doe@spichka.media>
2024-09-28 12:15:10 +0300

    fix: WP_Error namespace

--- themes/spichka/app/filters.php
+++ themes/spichka/app/filters.php
@@ -196,7 +196,7 @@ add_filter('rest_authentication_errors', function ($result) {
   // No authentication has been performed yet.
   // Return an error if user is not logged in.
   if (!is_user_logged_in()) {
-    return new WP_Error(
+    return new \WP_Error(
       'rest_not_logged_in',
       __('You are not currently logged in.'),
       ['status' => 401]
