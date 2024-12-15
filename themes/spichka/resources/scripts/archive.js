c1bd1c550fe9d169d3e851ea46651b342fece2de
John Doe <john.doe@spichka.media>
2024-12-15 20:03:17 +0200

    feat: remove pagination track

--- themes/spichka/resources/scripts/archive.js
+++ themes/spichka/resources/scripts/archive.js
@@ -1,24 +1,5 @@
-/* global gtag */
 import domReady from '@roots/sage/client/dom-ready';
 
 domReady(async () => {
-  registerPaginationLinkEvents();
+  //
 });
-
-function registerPaginationLinkEvents() {
-  if (typeof gtag !== 'function') {
-    return;
-  }
-
-  const paginationLinks = document.querySelectorAll('a.page-numbers.page-link');
-
-  paginationLinks.forEach((link) => {
-    link.addEventListener('click', function () {
-      gtag('event', 'pagination_click', {
-        event_category: 'Pagination',
-        event_label: this.href,
-        value: this.textContent,
-      });
-    });
-  });
-}
