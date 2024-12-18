62df2651d6c4cadef1b3b63ed1ed5b242385199c
John Doe <john.doe@spichka.media>
2024-12-18 15:54:26 +0200

    feat: add default quote

--- themes/spichka/resources/views/sections/footer.blade.php
+++ themes/spichka/resources/views/sections/footer.blade.php
@@ -7,7 +7,7 @@
             'id' => 'footer-image',
             'data-bs-toggle' => 'tooltip',
             'data-bs-placement' => 'right',
-            'data-bs-title' => "''",
+            'data-bs-title' => 'Только на кладбище осуществимо полное тождество взглядов',
             'data-bs-offset' => '0,20',
             'data-bs-custom-class' => 'portraits-tooltip',
           ])
