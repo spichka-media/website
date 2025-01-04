8a4e94af8fb7cae2b7a1d7c3435a1e71ef4e719c
John Doe <john.doe@spichka.media>
2025-01-04 16:03:06 +0200

    feat: fix tables

--- themes/spichka/resources/views/layouts/app.blade.php
+++ themes/spichka/resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 @include('sections.header')
 
-<main id="main" class="main d-flex flex-column h-100 flex-grow-1 text-break">
+<main id="main" class="main d-flex flex-column h-100 flex-grow-1">
   @yield('content')
 </main>
 
