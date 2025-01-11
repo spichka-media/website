db098b0886b0e6676473753718d74b9646d551f3
podvetrov <vladyslav.podvetrov@spichka.media>
2025-01-11 05:57:00 +0100

    Add call-to-action (#60)
    Co-authored-by: John Doe <john.doe@spichka.media>

--- themes/spichka/resources/scripts/lib/utils.js
+++ themes/spichka/resources/scripts/lib/utils.js
@@ -16,3 +16,17 @@ export function preloadImages(urls) {
     image.src = url;
   }
 }
+
+/**
+ * Checks whether any part visible
+ * @param Element
+ * @returns boolean
+ */
+export function checkVisible(elm) {
+  const rect = elm.getBoundingClientRect();
+  const viewHeight = Math.max(
+    document.documentElement.clientHeight,
+    window.innerHeight,
+  );
+  return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+}
