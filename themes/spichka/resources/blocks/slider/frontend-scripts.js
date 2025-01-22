c26bed2d5c2827e0baa58225bf7c27304a46b0f4
podvetrov <vladyslav.podvetrov@spichka.media>
2025-01-22 21:48:31 +0100

    about-us swiper: narrow selector

--- themes/spichka/resources/blocks/slider/frontend-scripts.js
+++ themes/spichka/resources/blocks/slider/frontend-scripts.js
@@ -2,8 +2,11 @@ import domReady from '@roots/sage/client/dom-ready';
 import {initSliders} from '../../scripts/lib/swiper.js';
 
 domReady(async () => {
-  initSliders({
-    centeredSlides: true,
-    centeredSlidesBounds: true,
-  });
+  initSliders(
+    {
+      centeredSlides: true,
+      centeredSlidesBounds: true,
+    },
+    '[data-swiper-type="article-images-block"]',
+  );
 });
