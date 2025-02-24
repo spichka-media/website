16ba6a0b92a82aa7f2bd827d380c8bb544a9f511
Николай Кулашенко <john.doe@spichka.media>
2025-02-24 11:58:08 +0200

    fix: issue with comments

--- themes/spichka/resources/views/partials/template-single-article.blade.php
+++ themes/spichka/resources/views/partials/template-single-article.blade.php
@@ -147,12 +147,12 @@
         </div>
       @endif
 
-      @if (carbon_get_post_meta(get_the_ID(), 'post_show_comments') && $telegramChannel)
+      @if (carbon_get_post_meta(get_the_ID(), 'post_show_comments') && $telegramChannel && $telegramPostId)
         <div class="col-12 col-lg-9 col-xl-8 px-xl-6">
           <script
             async
             src="https://telegram.org/js/telegram-widget.js?22"
-            data-telegram-discussion="{{ $telegramChannel . $telegramPostId ? '/' . $telegramPostId : '' }}"
+            data-telegram-discussion="{{ $telegramChannel . '/' . $telegramPostId }}"
             data-comments-limit="5"
             data-color="343638"
             data-dark-color="FFFFFF"></script>
