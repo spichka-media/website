ce24937a0ef995868409a3a639f5a83d609109fd
VaskillerDev <Vaskoooo9241@gmail.com>
2025-04-29 17:22:40 +0300

    +author_foreign_agent_prevention_text

--- themes/spichka/resources/views/partials/page-header.blade.php
+++ themes/spichka/resources/views/partials/page-header.blade.php
@@ -1,3 +1,13 @@
 <div class="page-header">
   <h1 class="my-0 mt-lg-6 mb-lg-3">{!! $title !!}</h1>
+
+  @if (is_author() && str_contains(get_the_author_meta('last_name'), '*'))
+    @php
+      $prevention_text = carbon_get_theme_option('author_foreign_agent_prevention_text');
+    @endphp
+
+    <p>
+      {{ get_the_author_meta('first_name') . ' ' . get_the_author_meta('last_name') . ' ' . $prevention_text }}
+    </p>
+  @endif
 </div>
