69404dc623ec8dda419de3aa19e2574ef1d07b10
VaskillerDev <Vaskoooo9241@gmail.com>
2025-04-30 23:20:20 +0300

    minor

--- themes/spichka/resources/views/partials/page-header.blade.php
+++ themes/spichka/resources/views/partials/page-header.blade.php
@@ -4,10 +4,11 @@
   @if (is_author() && str_contains(get_the_author_meta('last_name'), '*'))
     @php
       $prevention_text = carbon_get_theme_option('author_foreign_agent_prevention_text');
+      $processed_last_name = substr(get_the_author_meta('last_name'), 0, -1);
     @endphp
 
     <p>
-      {{ get_the_author_meta('first_name') . ' ' . get_the_author_meta('last_name') . ' ' . $prevention_text }}
+      {{ '*' . get_the_author_meta('first_name') . ' ' . $processed_last_name . ' ' . $prevention_text }}
     </p>
   @endif
 </div>
